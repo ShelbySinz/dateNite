012dba0e625c6aa7a7d366ece6c6cafbf7c63548
Armi <armi.avelino@gmail.com>
2019-04-21 15:19:45 -0600

    updated Nav, Jumbotron, added Restaurant, returned scrolling for List

--- client/src/components/NavBar/Nav.js
+++ client/src/components/NavBar/Nav.js
@@ -3,10 +3,26 @@ import React from "react";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-info">
-      <a className="navbar-brand" href="/">
-        DateNite
-      </a>
+        <a className="navbar-brand" href="/">
+          DateNite
+        </a>
+        <a className="navbar-brand" href="/dates/atHome">
+          At Home
+        </a>
+        <a className="navbar-brand" href="/dates/lowBudget">
+          Low Budget
+        </a>
+        <a className="navbar-brand" href="/dates/outdoors">
+          Outdoors
+        </a>
+        <a className="navbar-brand" href="/dates/restaurants">
+          Restaurants and Bars
+        </a>
+        <a className="navbar-brand" href="/dates/movies">
+          Movies and TV
+        </a>
     </nav>
+    
   );
 }
 
---
+++ client/src/pages/Restaurant/restaurantDate.js
@@ -0,0 +1,122 @@
+import React, {Component} from "react";
+import {FormBtn, Input} from "../../components/Form";
+import {List, ListItem} from "../../components/List";
+import {Container,Row, Col} from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron/Jumbotron";
+import API from "../../utils/API";
+import restaurantJson from "../../restaurants.json";
+import SaveBtn from "../../components/SaveBtn";
+
+class Restaurant extends Component {
+    state = {
+        restaurant: [],
+        search: "",      
+      };
+
+    //  componentDidMount(){
+    //    this.handleApiSubmit()
+    //  }
+
+      handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
+    
+      handleApiSubmit = query => {        
+        API.getRestaurant(query).then( res => this.setState({restaurant: res.data.businesses}))
+        console.log(this.state.restaurant)
+      }
+    
+      handleFormSubmit = event => {
+        event.preventDefault();
+        this.handleApiSubmit(this.state.search)
+        console.log(this.state.search);
+      }
+     
+    
+      render() {
+        return (
+          <Container fluid>
+            <Row>
+              <Col size="md-12">
+                <Jumbotron>
+                  <h1>Restaurant Date Ideas!</h1>
+                  <p>Spend the night out and spend some money!</p>
+                  </Jumbotron>
+
+                  </Col>
+                  </Row>
+                  <Row>
+                   <Col size= "md-12">
+                  <List>
+                      
+                    {restaurantJson.map( dates => {
+                      console.log(dates)
+                      
+                      return (
+                        <ListItem key={dates.title}  >
+                           
+                           <strong><h2>{dates.title}</h2></strong> 
+                           <p>{dates.description}</p>
+                           <p>What you Need: {dates.whatYouNeed}</p>
+                        
+                          <SaveBtn   
+                            onClick={() => API.saveDates({title: dates.title, type: dates.type, description: dates.description, whatYouNeed: dates.whatYouNeed})}                                     
+                          >Save</SaveBtn>
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                </Col>
+                  </Row>
+                  <br></br>
+                  <br></br>
+                  <Row>
+                 <Col size="md-6">
+                <form>
+                  <Input
+                    value={this.state.search}
+                    onChange={this.handleInputChange}
+                    name="search"
+                    placeholder="Denver, CO"
+                  />
+              
+                  <FormBtn
+                    onClick={this.handleFormSubmit}
+                  >
+                    Find Restaurant
+                  </FormBtn>
+                </form>
+              </Col>
+              <Col size="md-6 sm-12">
+                    <List>
+                    {/* {this.state.restaurant.map(result => (
+                      <ListItem key={result.id}  >                         
+                            <strong>Name: {result.name}</strong> 
+                            <p>Location: {result.location}</p>
+                            <p>Price: {result.price}</p>
+                            <br></br>
+                            <img src={result.image_url} alt={result.name}></img>
+                            <br></br>
+                            <br></br>
+                          <a href={result.url} target="_blank"><button className="btn btn-sm btn-primary" >view</button></a>                          
+                      </ListItem>
+                      ))} */}
+                      <ListItem key={this.state.restaurant.id}  >                         
+                            <strong>Name: {this.state.restaurant.name}</strong> 
+                            <p>Location: {this.state.restaurant.location}</p>
+                            <p>Price: {this.state.restaurant.price}</p>
+                            <img src={this.state.restaurant.image_url} alt={this.state.restaurant.name}></img>
+                          <a href={this.state.restaurant.url} target="_blank"><button className="btn btn-sm btn-primary" >view</button></a>                          
+                      </ListItem>
+                    </List>              
+              </Col>
+            </Row>
+          </Container>
+        );
+      }
+    }
+    
+    export default Restaurant;
