80d7cac667c49cfbe5b09ffd3a7fe0d948e9e1cc
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-25 13:44:45 -0600

    added authentication logic to App.js and routes for Register and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,69 @@
 
 import React, { Component } from "react";
-// import logo from "./logo.svg";
+
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+
+//import views
 import Home from "./pages/Home/home";
 import HomeDates from "./pages/atHome/homeDate";
 import MovieDates from "./pages/Movies/movieDate";
 import RestaurantDates from "./pages/Restaurant/restaurantDate";
 import Saved from "./pages/Saved/saved.js";
 import NoMatch from "./pages/NoMatch";
+
+//import componenets
 import Nav from "./components/NavBar/Nav";
-// import Low from "./pages/low-budget/lowBudget"
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  
+  setAuthToken(token);
+  
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+ 
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <div className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h2>Welcome to DateNite</h2>
-      //   </div>
-      //   <p className="App-intro">
-      //     What do uou feel like doing on your date?
-      //   </p>
-      // </div>
-      <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-           <Route exact path="/dates/atHome" component={HomeDates} /> 
-           <Route exact path="/dates/movies" component={MovieDates}/>
-           <Route exact path="/dates/restaurants" component={RestaurantDates}/>
-          <Route exact path="/dates/saved" component={Saved} /> 
-          {/* <Route exact path ="/dates/" component= {Low}/> */}
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
+      <Provider store={store}>
+        <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dates/atHome" component={HomeDates} /> 
+            <Route exact path="/dates/movies" component={MovieDates}/>
+            <Route exact path="/dates/restaurants" component={RestaurantDates}/>
+            <Route exact path="/dates/saved" component={Saved} /> 
+            <Route component={NoMatch} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 
   }
