fc8c8b3fccaf56e0fdb2f6b4719d09f97fab3375
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-26 17:29:37 -0600

    added state for outdoor component

--- client/src/pages/Outdoor/outdoorDate.js
+++ client/src/pages/Outdoor/outdoorDate.js
@@ -5,17 +5,28 @@ import Jumbotron from "../../components/Jumbotron/Jumbotron";
 import API from "../../utils/API";
 
 //create component 
+class Outdoor extends Component {
+		//set state
+		state = {
+			trails: [],
+			name: "",
+			summary: "",
+			difficulty: "",
+			location: "",
+			imgSqSmall: "",
+			conditionDetails: ""
+		};
+    
+    //save date function
+    
+    //handle input change, handle updating component state when the user types into input field
+    
+    //handle api/query params function
+    
+    //handle form submit function
+    
+    //render and return container, make use of layout components
 
-//set state
+}
 
-//save date function
-
-//handle input change, handle updating component state when the user types into input field
-
-//handle api/query params function
-
-//handle form submit function
-
-//render and return container, make use of layout components
-
-//export the component
+//export the components
