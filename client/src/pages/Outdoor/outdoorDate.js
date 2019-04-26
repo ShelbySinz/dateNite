d4f96cd091ba1d3b795e3e0bf0e6cbb1a95a3d4b
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-26 16:56:14 -0600

    added pseudo commenting for creation of component

--- client/src/pages/Outdoor/outdoorDate.js
+++ client/src/pages/Outdoor/outdoorDate.js
@@ -3,3 +3,19 @@ import {List, ListItem} from "../../components/List";
 import {Container, Row, Col} from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron/Jumbotron";
 import API from "../../utils/API";
+
+//create component 
+
+//set state
+
+//save date function
+
+//handle input change, handle updating component state when the user types into input field
+
+//handle api/query params function
+
+//handle form submit function
+
+//render and return container, make use of layout components
+
+//export the component
