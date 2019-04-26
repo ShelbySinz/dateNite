acc8b7ad05ec0d14c0a8bf4486cd703163cf076b
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-26 16:51:20 -0600

    added component imports in outdoorDate.js

---
+++ client/src/pages/Outdoor/outdoorDate.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+import {List, ListItem} from "../../components/List";
+import {Container, Row, Col} from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron/Jumbotron";
+import API from "../../utils/API";
