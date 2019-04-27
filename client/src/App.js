23e4321d511d0033150c0e4285eef42d5a9d884c
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-27 09:32:48 -0600

    uncommented saved route and nav

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
           <Route exact path="/dates/movies" component={MovieDates}/>
           <Route exact path="/dates/restaurants" component={RestaurantDates}/>
           <Route exact path="/dates/outdoors" component={OutdoorDates}/>
-          {/* <Route exact path="/dates/saved" component={Saved} />  */}
+          <Route exact path="/dates/saved" component={Saved} /> 
           {/* <Route exact path ="/dates/" component= {Low}/> */}
           <Route component={NoMatch} />
         </Switch>
