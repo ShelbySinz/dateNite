a3e352827032c6e2c6b125a28d827c857cb937a7
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-27 11:58:47 -0600

    update before master pull

---
+++ client/src/pages/Login/LoginPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Jumbotron1 from "../../components/Jumbotron/Jumbotron1";
+import Card from "../../components/Jumbotron/card";
+import {Container, Col, Row} from "../../components/Grid";
+import {Link} from "react-router-dom";
+import LoginPortal from "../../components/LoginPortal";
+
+function LoginPage() {
+    
+}
