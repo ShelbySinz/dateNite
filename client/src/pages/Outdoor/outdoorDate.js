c569133d1b68c1235ff0acf497ee7a6bd41cf74a
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-26 20:43:39 -0600

    Added boiler plate handle functions inside component     -handle input     -handle save date     -handle form submit     -handle state changes

--- client/src/pages/Outdoor/outdoorDate.js
+++ client/src/pages/Outdoor/outdoorDate.js
@@ -11,6 +11,7 @@ class Outdoor extends Component {
 		//set state
 		state = {
 			trails: [],
+			search: "",
 			name: "",
 			summary: "",
 			difficulty: "",
@@ -19,16 +20,123 @@ class Outdoor extends Component {
 			conditionDetails: ""
 		};
     
-    //save date function
-    
     //handle input change, handle updating component state when the user types into input field
-    
-    //handle api/query params function
-    
-    //handle form submit function
-    
-    //render and return container, make use of layout components
+    handleInputChange = event => {
+			const { name, value } = event.target;
+			this.setState({
+				[name]: value
+			});
+		};
 
+    //handle api/query params function
+		handleApiSubmit = query => {        
+      API.getRestaurant(query).then( res => this.setState({trails: res.data.results}));
+      console.log(this.state.trails);
+		};
+		
+		//handle form submit function
+		handleFormSubmit = event => {
+			event.preventDefault();
+			this.handleApiSubmit(this.state.search);
+			console.log(this.state.search);
+		};
+    
+    //save date function
+		SaveDates = () => {
+			const dateData={title: this.state.name, type: this.state.difficulty, 
+				summary: this.state.summary, conditionDetails: this.state.conditionDetails};
+			API.saveDates(dateData); 
+		 	console.log(this.state.name);
+	 }
+		
+		//render and return container, make use of layout components
+		render() {
+			return (
+				<Container fluid>
+					<Row>
+						<Col size="md-12">
+							<Jumbotron>
+								<h1>Outdoor Date Ideas!</h1>
+								<p>Spend the day outside and get to know eachother! Use the search bar below to find the best hiking trails near you or try one of our crafted activities!</p>
+								</Jumbotron>
+
+								</Col>
+								</Row>
+								<Row>
+								 <Col size= "md-8">
+								<List>                
+									{restaurantJson.map( dates => {
+										console.log(dates)
+										
+										return (
+											<ListItem key={dates.title}  >
+												<small className = "float-right">{dates.type}</small>
+												<br></br>
+												 
+												 <strong><h2>{dates.title}</h2></strong> 
+												 <p>{dates.description}</p>
+												 <p>What you Need: {dates.whatYouNeed}</p>
+											
+												<SaveBtn   
+													onClick={() => this.setState({title:dates.title, type:dates.type, description:dates.description, whatYouNeed:dates.whatYouNeed},this.SaveDates)}                                     
+												>Save</SaveBtn>
+											</ListItem>
+										);
+									})}
+								</List>
+							</Col>
+								<Col size= "md-4">
+								<img className={"img-fluid"} src="https://images-na.ssl-images-amazon.com/images/I/81UUBibOQvL._RI_.jpg" alt="Date Night"></img>
+								
+								</Col>
+								</Row>
+								<br></br>
+								<br></br>
+								<Row>
+							 <Col size="md-6">
+							<form>
+								<Input
+									value={this.state.search}
+									onChange={this.handleInputChange}
+									name="search"
+									placeholder="Mexican"
+								/>
+						
+								<FormBtn
+									onClick={this.handleFormSubmit}
+								>
+									Find Restaurant
+								</FormBtn>
+							</form>
+						</Col>
+						<Col size="md-6 sm-12">
+							{this.state.restaurant.length ? (
+									<List>
+
+									{this.state.restaurant.map(result => (
+										
+										<ListItem key={result.id}  >                         
+													<strong>Name: {result.name}</strong> 
+													<p>Location: {result.formatted_address}</p>
+													<p>Price (4 is very expensive!): {result.price_level}</p>
+													<p>Rating (out of 5 stars!): {result.rating}</p>                           
+													<a className="float-right" href={"https://www.google.com/maps/place/" + result.formatted_address} target="_blank"><i className="fas fa-eye"></i></a>
+													<a className="float-left" href={"mailto:?body= Address to"+ result.name + ":" + result.formatted_address} target="_top"><i className="far fa-envelope"></i></a>
+										</ListItem>
+										))}
+									</List>   
+									 ) : (
+										<h3>No Results to Display</h3>
+									 )}              
+						</Col>
+					</Row>
+				</Container>
+			);
+		}
+	}
+	
+	export default Restaurant;
+		
 }
 
 //export the components
